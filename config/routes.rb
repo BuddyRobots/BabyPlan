509986ef57815f70c23b2d05ce26be1bed3c5a7e
alan <wenqiang@buddyrobots.com>
2016-06-14 09:52:40 +0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/test'
+  get 'welcome/test1'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
