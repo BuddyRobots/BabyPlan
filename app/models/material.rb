d06aa3eaea2f787cfa2fb47d35d491be64d69ca6
alan <wenqiang@buddyrobots.com>
2016-08-29 10:19:19 +0800

    829-1

--- app/models/material.rb
+++ app/models/material.rb
@@ -6,4 +6,7 @@ class Material
   field :path, type: String
   field :type, type: String
 
+  belongs_to :cover_book, class_name: "Book", inverse_of: :cover
+  belongs_to :back_book, class_name: "Book", inverse_of: :back
+
 end
