8fc46155b27cc736cf138753ce9f1bf22ffd35fd
JesseYang <jesse.yang1985@gmail.com>
2016-09-19 11:36:43 +0800

    fix bug

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -2,7 +2,7 @@ class MaterialsController < ApplicationController
 
   def create
     image = Image.new
-    image.image = params[:wangEditorPasteFile]
+    image.image = params[:wangEditorH5File] || params[:wangEditorPasteFile]
     image.store_image!
     filepath = image.image.file.file
     render text: "/uploads/images/" + filepath.split('/')[-1] and return
