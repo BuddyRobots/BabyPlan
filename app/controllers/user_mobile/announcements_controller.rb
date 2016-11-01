57cf348e86244373219a57262b6cfcbceb769014
JesseYang <jesse.yang1985@gmail.com>
2016-11-01 17:12:12 +0800

    u

--- app/controllers/user_mobile/announcements_controller.rb
+++ app/controllers/user_mobile/announcements_controller.rb
@@ -1,10 +1,15 @@
 class UserMobile::AnnouncementsController < UserMobile::ApplicationController
   # similar to search_new
   def index
-    
+    if @current_user.client_centers.present?
+      @announcements = Announcement.any_in(center_id: @current_user.client_centers.map { |e| e.id.to_s})
+      if params[:keyword].present?
+        @announcements = @announcements.where(name: /#{params[:keyword]}/)
+      end
+    end
   end
+
   # noticedescription
   def show
-    
   end
 end
