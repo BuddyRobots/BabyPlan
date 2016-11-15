ee0b75558501521b804a73005df884304bfc471f
JesseYang <jesse.yang1985@gmail.com>
2016-11-15 08:36:55 +0800

    feeds page

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -12,6 +12,35 @@ class Feed
 
 
   def center_str
-  	
+    if self.announcement.present? && self.center.blank?
+      return "所有儿童中心"
+    end
+    if self.center.present?
+      return self.center.name
+    else
+      return ""
+    end
+  end
+
+  def desc
+    if self.course_inst.present?
+      self.course_inst.desc
+    elsif self.book.present?
+      self.book.desc
+    elsif self.announcement.present?
+      self.announcement.content
+    else
+      ""
+    end
+  end
+
+  def feed_icon
+    if self.course_inst.present?
+      "ke"
+    elsif self.book.present?
+      "hui"
+    else
+      "gao"
+    end
   end
 end
