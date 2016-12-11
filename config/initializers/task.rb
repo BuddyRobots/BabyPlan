3e879fe9abcdb69de147050cb78b5feb16090b6b
JesseYang <jesse.yang1985@gmail.com>
2016-12-12 00:14:27 +0800

    u

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -5,3 +5,11 @@ scheduler.every("1d") do
   # puts Time.now
   Statistic.calculate_daily_stats
 end
+
+scheduler.every("1d") do
+  CourseParticipate.all.each do |cp|
+    if cp.refund_finished == true && !%w[CourseParticipate::SUCCESS, CourseParticipate::FAIL, CourseParticipate::CHANGE].include?(cp.refund_status)
+      cp.refundquery
+    end
+  end
+end
