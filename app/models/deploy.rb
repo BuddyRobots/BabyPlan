5ee11a83a4152636df9906cad99b496e388548a7
JesseYang <jesse.yang1985@gmail.com>
2016-12-13 10:17:04 +0800

    u

---
+++ app/models/deploy.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+class Deploy
+
+
+  def self.deploy
+    `cd ~/Projects/BabyPlan`
+    pull_result = `git pull origin practice`
+    bundle_result = `bundle`
+    compile_result = `RAILS_ENV=production rake assets:precompile`
+    `touch tmp/restart`
+    {
+      pull_result: pull_result,
+      bundle_result: bundle_result,
+      compile_result: compile_result
+    }
+  end
+end
