3572bee3d4dab9f6e704da0c8656008b993370ad
JesseYang <jesse.yang1985@gmail.com>
2016-12-19 11:48:36 +0800

    u

--- app/models/deploy.rb
+++ app/models/deploy.rb
@@ -2,7 +2,7 @@
 class Deploy
 
   def self.deploy(address)
-    pull_result = `git pull origin practice`
+    pull_result = `git pull origin deploy`
     bundle_result = `bundle`
     `RAILS_ENV=production rake assets:precompile`
     `touch tmp/restart`
@@ -16,10 +16,19 @@ class Deploy
     # first copy database from production server
     `mongodump --db baby_plan_production --username db_user --password Brs@2016`
     `mongorestore --db baby_plan_staging --username db_user --password Brs@2016 --drop dump/baby_plan_production`
-    pull_result = `git pull origin practice`
+    pull_result = `git pull origin deploy`
     bundle_result = `bundle`
     `RAILS_ENV=production rake assets:precompile`
     `touch tmp/restart`
+    {
+      pull_result: pull_result,
+      bundle_result: bundle_result
+    }
+  end
+
+  def self.deploy_practice
+    pull_result = `git pull origin practice`
+    bundle_result = `bundle`
     {
       pull_result: pull_result,
       bundle_result: bundle_result
