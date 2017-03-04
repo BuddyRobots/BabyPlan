36692e3adcb4953e626e20f3a66a3af00559e196
alan <wenqiang@buddyrobots.com>
2017-03-04 12:18:56 +0800

    root routes

--- config/routes.rb
+++ config/routes.rb
@@ -321,7 +321,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sessions#signin_page'
+  root 'user_mobile/sessions#signin'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
