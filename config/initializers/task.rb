f4a253537e1309537546ce4c25a799860edbbfb1
JesseYang <jesse.yang1985@gmail.com>
2017-04-19 14:27:30 +0800

    u

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -1,12 +1,19 @@
 require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
  
-scheduler.every("1d") do
-  # puts Time.now
-  Statistic.calculate_daily_stats
+# scheduler.every("1d") do
+#   # puts Time.now
+#   Statistic.calculate_daily_stats
+# end
+
+# scheduler.cron '0 12 * * *' do
+scheduler.every '3s' do
+  # Rails.logger.info "AAAAAAAAA"
+  # print "BBBBBBBB"
 end
 
 scheduler.every("1d") do
+
   CourseParticipate.all.each do |cp|
     if cp.refund_finished == true && !%w[CourseParticipate::SUCCESS, CourseParticipate::FAIL, CourseParticipate::CHANGE].include?(cp.refund_status)
       cp.refundquery
