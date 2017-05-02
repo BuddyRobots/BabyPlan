ba8701d53a1f75547c31dbb83ea64f6dcdbaf6af
alan <wenqiang@buddyrobots.com>
2017-05-02 11:21:35 +0800

    review

--- app/controllers/user_mobile/reviews_controller.rb
+++ app/controllers/user_mobile/reviews_controller.rb
@@ -5,7 +5,7 @@ class UserMobile::ReviewsController < UserMobile::ApplicationController
     @course_inst = CourseInst.where(id: params[:course_inst_id]).first
     @cp = @course_inst.course_participates.where(client_id: @current_user.id).first
     @self_review = (@cp.present? && @cp.trade_state == "SUCCESS") && @current_user.reviews.where(course_inst_id: @course_inst.id).blank?
-    reviews = @course_inst.reviews.where(status: 1).desc(:created_at)
+    reviews = @course_inst.reviews.where(status: PUBLIC).desc(:created_at)
     @ele = @course_inst
     @reviews = auto_paginate(reviews)[:data]
   end
@@ -24,7 +24,7 @@ class UserMobile::ReviewsController < UserMobile::ApplicationController
 
   def more
     @course_inst = CourseInst.where(id: params[:course_id]).first
-    @reviews = @course_inst.reviews.where(status: 1).desc(:created_at)
+    @reviews = @course_inst.reviews.where(status: PUBLIC).desc(:created_at)
     @reviews = auto_paginate(@reviews)[:data]
     @reviews = @reviews.map { |r| r.more_info }
     render json: retval_wrapper({more: @reviews}) and return
