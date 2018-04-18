08315f7ab1c4f52c2a9cfffb4c5e7f837f2f4bec
JesseYang <jesse.yang1985@gmail.com>
2018-04-18 10:49:38 +0800

    remove mobile check

--- app/assets/javascripts/utility/regex.js
+++ app/assets/javascripts/utility/regex.js
@@ -15,7 +15,8 @@
 			return /^((\d{11,12})|(\d{3,4}-\d{8})|(\d{4}-\d{7})|(\d{3,4}-\d{4}-\d{4}))$/.test(phone);
 		},
 		isMobile: function(mobile) {
-			return (/^(13[0-9]|15[012356789]|18[0-9]|14[57]|170)[0-9]{8}$/.test(mobile));
+			// return (/^(13[0-9]|15[012356789]|18[0-9]|14[57]|170)[0-9]{8}$/.test(mobile));
+			return true;
 		},
 
 		isYidongNumber:function(number){
